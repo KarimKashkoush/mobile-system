f9db452853c5b05af319b4dd57ad5ad6d0bedfb1
KarimKashkoush <karimkashkoush5@gmail.com>
2024-11-18 02:48:06 +0200

    add

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  server: {
-    historyApiFallback: true, // تفعيل إعادة توجيه المسارات إلى index.html
-  },
-});
+  plugins: [react()],
+})
